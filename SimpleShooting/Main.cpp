66c06f50c48f0ee6ee78cf7687cdf6bbab36743b
Chisaka <a1502020@sendai-nct.jp>
2016-06-23 11:02:16 +0900

    敵がランダムで出現するようにした

--- SimpleShooting/Main.cpp
+++ SimpleShooting/Main.cpp
@@ -11,11 +11,8 @@ void Main()
 	// 自機 { 初期位置, 半径, 色, 速度 }
 	OwnChar me = { { 320, 240 }, 20,{ 255, 0, 0, 255 }, 6 };
 
-	// 敵機 { 初期位置, 半径, 色, 向き, 速度 }
-	list<EnemyChar> enemies = {
-		{ { 100, 0 }, 20, { 255, 0, 255, 255 }, Pi * 0.5, 2 },
-		{ { 540, 0 }, 20, { 255, 0, 255, 255 }, Pi * 0.5, 2 }
-	};
+	// 敵 { 初期位置, 半径, 色, 向き, 速度 }
+	list<EnemyChar> enemies;
 
 	while (System::Update())
 	{
@@ -25,7 +22,7 @@ void Main()
 		if (Input::KeyLeft.pressed) me.moveLeft();
 		if (Input::KeyDown.pressed) me.moveDown();
 
-		// 敵機移動
+		// 敵移動
 		for (EnemyChar &e : enemies)
 		{
 			e.move();
@@ -35,6 +32,16 @@ void Main()
 		enemies.remove_if([&](const EnemyChar &e) {
 			return !Window::ClientRect().intersects(Circle(e.p, e.r));
 		});
+
+		// 敵出現
+		if (RandomBool(1.0 / 40)) {
+			int32 r = Random(15, 30);
+			Point p = RandomPoint({ r, r, Window::Width() - 2 * r, 1 });
+			Color col = { 255, 0, 255, 255 };
+			double angle = Random(Pi * 0.25, Pi * 0.75);
+			int32 v = Random(2, 5);
+			enemies.push_back({ p, r, col, angle, v });
+		}
 
 		// 描画
 		Circle(me.p, me.r).draw(me.col);
